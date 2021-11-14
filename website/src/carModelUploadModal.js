7a1c8aa2ed15fbb23515d1c9d1e6e462a26cc678
Steven Askwith <askwith@amazon.com>
2021-11-14 21:34:06 +0000

    added result modal

--- website/src/carModelUploadModal.js
+++ website/src/carModelUploadModal.js
@@ -1,65 +1,109 @@
-import React, {useState} from 'react'
+import React, { Component } from 'react';
 import { API } from 'aws-amplify';
 import { Table, Button, Modal } from 'semantic-ui-react'
 
-async function uploadModelToCar(car, model) {
-  console.log(car.InstanceId)
-  console.log(model.key)
 
-  const apiName = 'deepracerEventManager';
-  const apiPath = 'cars/upload';
-  const myInit = { 
-    body: {
-      InstanceId: car.InstanceId,
-      key: model.key
-    }
-  };
+class CarModelUploadModal extends Component {
+  constructor(props) {
+    super(props);
+    this.containerDiv = React.createRef();
+    this.state = {
+      open: false,
+      resultOpen: false,
+      result: "",
+    };
+  }
 
-  let response = await API.post(apiName, apiPath, myInit);
-  console.log(response)
-  return response
-}
+  componentDidMount = async () => {
+    this.setState({ result: <p>Mounted</p> })
+  }
 
-function CarModelUploadModal(props) {
-  const [open, setOpen] = useState(false)
+  uploadModelToCar= async (car, model) => { 
+    console.log(car.InstanceId)
+    console.log(model.key)
 
-  //let cars = collectCars()
-  //let cars = [];
-  //console.log(cars.cars);
-  //console.log('model')
-  //console.log(props.model)
+    const apiName = 'deepracerEventManager';
+    const apiPath = 'cars/upload';
+    const myInit = { 
+      body: {
+        InstanceId: car.InstanceId,
+        key: model.key
+      }
+    };
 
-  var modaltablerows = props.cars.map(function (car, i) {
-    return <Table.Row key={i} >
-      <Table.Cell>{car.ComputerName} </Table.Cell>
-      <Table.Cell><Button content="Upload" labelPosition='right' icon='upload' onClick={() => {setOpen(false); uploadModelToCar(car, props.model) }} positive /></Table.Cell>
-    </Table.Row>
-  });
+    let response = await API.post(apiName, apiPath, myInit);
+    console.log(response)
+    this.setState({ result: response });
+    return response
+  }
 
-  return (
-    
-    <Modal
-      onClose={() => setOpen(false)}
-      onOpen={() => setOpen(true)}
-      open={open}
-      trigger={<Button positive circular icon='upload' />}
-    >
-      <Modal.Header>Select a Car</Modal.Header>
-      <Modal.Content>
-        <p>
-          Pick a car
-        </p>
-        <Table>
-          <Table.Body>
-            {modaltablerows}
-          </Table.Body>
-        </Table>
-      </Modal.Content>
-      <Modal.Actions>
-        <Button color='red' onClick={() => setOpen(false)}>Close</Button>
-      </Modal.Actions>
-    </Modal>
-  )
+  render(){
+    //let cars = collectCars()
+    //let cars = [];
+    //console.log(cars.cars);
+    //console.log('model')
+    //console.log(props.model)
+
+    var modaltablerows = this.props.cars.map(function (car, i) {
+      return <Table.Row key={i} >
+        <Table.Cell>{car.ComputerName} </Table.Cell>
+        <Table.Cell><Button content="Upload" labelPosition='right' icon='upload' onClick={() => {
+          this.setState({ result: <p>Uploading...</p> });
+          this.setState({ open: false });
+          this.setState({ resultOpen: true }); 
+          this.uploadModelToCar(car, this.props.model) 
+          }} positive /></Table.Cell>
+      </Table.Row>
+    }.bind(this));
+
+    return (
+      <>
+        <Modal
+          onClose={() => this.setState({ open: false })}
+          onOpen={() => this.setState({ open: true })}
+          open={this.state.open}
+          trigger={<Button positive circular icon='upload' />}
+        >
+          <Modal.Header>Select a Car</Modal.Header>
+          <Modal.Content>
+            <p>
+              Pick a car
+            </p>
+            <Table>
+              <Table.Body>
+                {modaltablerows}
+              </Table.Body>
+            </Table>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button color='red' onClick={() => {
+              this.setState({ open: false }); 
+              this.setState({ result: "" });
+            }}>Close</Button>
+          </Modal.Actions>
+        </Modal>
+
+        <Modal
+        onClose={() => this.setState({ resultOpen: false })}
+        onOpen={() => this.setState({ resultOpen: true })}
+        open={this.state.resultOpen}
+        >
+        <Modal.Header>Upload Result</Modal.Header>
+        <Modal.Content>
+          <p>
+            {this.state.result}
+          </p>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button color='red' onClick={() => {
+            this.setState({ resultOpen: false }); 
+            this.setState({ result: "" });
+          }}>Close</Button>
+        </Modal.Actions>
+        </Modal>
+      </>
+    )
+  }
 }
 
 export default CarModelUploadModal
