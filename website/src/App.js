030f01d85b08ea186401928a6e01bb22180b4262
Steven Askwith <askwith@amazon.com>
2021-11-24 00:19:21 +0000

    updated to @aws-amplify/ui-react 2.x

--- website/src/App.js
+++ website/src/App.js
@@ -2,40 +2,26 @@ import './App.css';
 import React from 'react';
 import awsconfig from './config.json';
 import Amplify from 'aws-amplify';
-import { AmplifyAuthenticator, AmplifySignOut, AmplifySignIn } from '@aws-amplify/ui-react';
-import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
-import { Header } from 'semantic-ui-react';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import './amplify.css';
 
 import { Menu } from './menu.js';
+import { Header, Button } from 'semantic-ui-react'
 
 Amplify.configure(awsconfig);
 
 
-function App() {
-  const [authState, setAuthState] = React.useState();
-  const [user, setUser] = React.useState();
-
-  React.useEffect(() => {
-      onAuthUIStateChange((nextAuthState, authData) => {
-          setAuthState(nextAuthState);
-          setUser(authData)
-      });
-  }, []);
-
-  return authState === AuthState.SignedIn && user ? (
-      <div className="App">
-          <Header as='h1'>Hello {user.username}</Header>
+export default function App() {
+  return (
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <Header as='h1' icon textAlign='center'>Hello {user.username}</Header>
           <Menu />
-          <AmplifySignOut />
-      </div>
-    ) : (
-      <AmplifyAuthenticator>
-        <AmplifySignIn // https://docs.amplify.aws/ui/auth/authenticator/q/framework/react#sign-in
-          hideSignUp="true"
-          slot="sign-in"
-        ></AmplifySignIn>
-      </AmplifyAuthenticator>
-    );
+          <Button fluid onClick={signOut}>Sign out</Button>
+        </main>
+      )}
+    </Authenticator>
+  );
 }
-
-export default App;
