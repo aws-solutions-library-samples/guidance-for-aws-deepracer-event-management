ec72d6d5645c6084a74eb3c27503a3c6f6c933f6
Steven Askwith <askwith@amazon.com>
2022-04-15 20:35:35 +0100

    selectable rows

--- website/src/admin/models2.js
+++ website/src/admin/models2.js
@@ -3,21 +3,29 @@ import { API } from 'aws-amplify';
 import { Container, Header, Table, Checkbox, } from 'semantic-ui-react';
 
 import CarModelUploadModal from "./carModelUploadModal.js";
-import { useTable, useSortBy } from 'react-table'
+import { useTable, useSortBy, useRowSelect } from 'react-table'
 
-async function getCars() {
-  console.log("Collecting cars...")
+const IndeterminateCheckbox = React.forwardRef(
+  ({ indeterminate, ...rest }, ref) => {
+    const defaultRef = React.useRef()
+    const resolvedRef = ref || defaultRef
 
-  const apiName = 'deepracerEventManager';
-  const apiPath = 'cars';
+    React.useEffect(() => {
+      resolvedRef.current.indeterminate = indeterminate
+    }, [resolvedRef, indeterminate])
 
-  const response = await API.get(apiName, apiPath);
-  //console.log(response);
-  return response;
-}
+    return (
+      <>
+        <input type="checkbox" ref={resolvedRef} {...rest} />
+      </>
+    )
+  }
+)
 
 function AdminModels2() {
   const [data, setData] = useState([]);
+  const [cars, setCars] = useState([]);
+
 
   useEffect(() => {
     async function getModels() {
@@ -29,9 +37,20 @@ function AdminModels2() {
       const response = await API.get(apiName, apiPath);
       setData(response);
     }
+
+    // async function getCars() {
+    //   console.log("Collecting cars...")
+    
+    //   const apiName = 'deepracerEventManager';
+    //   const apiPath = 'cars';
+    
+    //   const response = await API.get(apiName, apiPath);
+    //   setCars(response);
+    // }
+
     getModels();
+    //getCars();
   },[])
-  //setData(getModels());
 
   const columns = React.useMemo(
     () => [
@@ -59,55 +78,90 @@ function AdminModels2() {
     headerGroups,
     rows,
     prepareRow,
+    selectedFlatRows,
+    state: { selectedRowIds },
   } = useTable(
-    { columns, data },
-    useSortBy
+    {
+      columns,
+      data,
+    },
+    useSortBy,
+    useRowSelect,
+    hooks => {
+      hooks.visibleColumns.push(columns => [
+        // Let's make a column for selection
+        {
+          id: 'selection',
+          // The header can use the table's getToggleAllRowsSelectedProps method
+          // to render a checkbox
+          Header: ({ getToggleAllRowsSelectedProps }) => (
+            <div>
+              <IndeterminateCheckbox {...getToggleAllRowsSelectedProps()} />
+            </div>
+          ),
+          // The cell can use the individual row's getToggleRowSelectedProps method
+          // to the render a checkbox
+          Cell: ({ row }) => (
+            <div>
+              <IndeterminateCheckbox {...row.getToggleRowSelectedProps()} />
+            </div>
+          ),
+        },
+        ...columns,
+      ])
+    }
   )
 
   return (
-    <table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
-      <thead>
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                {column.render('Header')}
-                <span>
-                  {column.isSorted
-                    ? column.isSortedDesc
-                      ? ' 🔽'
-                      : ' 🔼'
-                    : ''}
-                </span>
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map(row => {
-          prepareRow(row)
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map(cell => {
-                return (
-                  <td
-                    {...cell.getCellProps()}
-                    style={{
-                      padding: '10px',
-                      border: 'solid 1px gray',
-                      background: 'papayawhip',
-                    }}
-                  >
-                    {cell.render('Cell')}
-                  </td>
-                )
-              })}
+    <>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render('Header')}
+                  <span>
+                    {column.isSorted
+                      ? column.isSortedDesc
+                        ? ' 🔽'
+                        : ' 🔼'
+                      : ''}
+                  </span>
+                </th>
+              ))}
             </tr>
-          )
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      <p>Selected Rows: {Object.keys(selectedRowIds).length}</p>
+      <pre>
+        <code>
+          {JSON.stringify(
+            {
+              selectedRowIds: selectedRowIds,
+              'selectedFlatRows[].original': selectedFlatRows.map(
+                d => d.original
+              ),
+            },
+            null,
+            2
+          )}
+        </code>
+      </pre>
+    </>
   )
 }
 
