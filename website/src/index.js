2d54952ff184f6c3067ae3762eca3793fde83b17
David Smith <dasmthc@amazon.co.uk>
2022-08-14 17:18:42 +0100

    Updated to reflect base app template

--- website/src/index.js
+++ website/src/index.js
@@ -5,11 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'semantic-ui-css/semantic.min.css'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
