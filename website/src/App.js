a2010cd5aac7e5e48de9f13bc2571713341c1bab
Steven Askwith <askwith@amazon.com>
2022-09-28 16:00:40 +0100

    Terms and Conditions signup validation

--- website/src/App.js
+++ website/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React from 'react';
 import awsconfig from './config.json';
 import Amplify from 'aws-amplify';
-import { Authenticator, View, useTheme } from '@aws-amplify/ui-react';
+import { Authenticator, View, useTheme, useAuthenticator, CheckboxField, Link } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 import FixedMenuLayout from './FixedMenuLayout.js';
@@ -20,11 +20,58 @@ const components = {
       </View>
     );
   },
+  
+  SignUp: {
+    FormFields() {
+      const { validationErrors } = useAuthenticator();
+
+      return (
+        <>
+          {/* Re-use default `Authenticator.SignUp.FormFields` */}
+          <Authenticator.SignUp.FormFields />
+
+          {/* Append & require Terms & Conditions field to sign up  */}
+          <CheckboxField
+            errorMessage={validationErrors.acknowledgement}
+            hasError={!!validationErrors.acknowledgement}
+            name="acknowledgement"
+            value="yes"
+            label="I agree with the Terms & Conditions"
+          />
+        </>
+      );
+    }
+  },
+
+  Footer() {
+    const { tokens } = useTheme();
+
+    return (
+      <View textAlign="center" padding={tokens.space.large}>
+        <Link href="/terms">
+          Terms and Conditions
+        </Link>
+      </View>
+    );
+  },
 }
 
 export default function App() {
   return (
-    <Authenticator components={components} hideSignUp={false} signUpAttributes={['email']}> 
+    <Authenticator 
+      components={components} 
+      services={{
+        async validateCustomSignUp(formData) {
+          if (!formData.acknowledgement) {
+            return {
+              acknowledgement: 'You must agree to the Terms & Conditions',
+            };
+          }
+        },
+      }}
+      hideSignUp={false} 
+      signUpAttributes={['email']}
+    > 
       {({ signOut, user }) => (
         <main>
           <FixedMenuLayout user={user.username} signout={signOut}/>
