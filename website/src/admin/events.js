638fd628778a234295300fc37a929e2473b22987
Steven Askwith <askwith@amazon.com>
2022-10-30 23:14:07 +0000

    Added basic events form

---
+++ website/src/admin/events.js
@@ -0,0 +1,133 @@
+import React, { useEffect, useState } from 'react';
+import { API, graphqlOperation } from 'aws-amplify';
+import * as queries from '../graphql/queries';
+import * as mutations from '../graphql/mutations';
+
+import { ContentHeader } from '../components/ContentHeader';
+
+import {
+  Button,
+  Form,
+  Header,
+  Grid,
+  SpaceBetween,
+  FormField,
+  Input,
+  Container,
+  ExpandableSection,
+  Table,
+  Alert,
+} from '@cloudscape-design/components';
+
+export function AdminEvents() {
+  const [events, setEvents] = useState([]);
+  const [newEvent, setNewEvent] = useState('');
+
+  useEffect(() => {
+    // Get Events
+    async function getAllEvents() {
+      const response = await API.graphql({
+        query: queries.getAllEvents
+      });
+      console.log('getAllEvents');
+      console.log(response.data.getAllEvents);
+      setEvents(response.data.getAllEvents);
+    }
+    getAllEvents();
+
+    return () => {
+      // Unmounting
+    }
+  }, [])
+
+  async function addEvent(newEvent) {
+    console.log(newEvent);
+    const response = await API.graphql({
+      query: mutations.addEvent,
+      variables: {
+        eventName: newEvent
+      }
+    });
+    console.log('addEvent');
+    console.log(response.data.addEvent);
+    return response;
+  }
+
+  return (
+    <>
+      <ContentHeader
+        header="Events admin"
+        description="List of Events."
+        breadcrumbs={[
+          { text: "Home", href: "/" },
+          { text: "Admin", href: "/admin/home" },
+          { text: "Groups" }
+        ]}
+      />
+      <Grid gridDefinition={[{ colspan: 1 }, { colspan: 10 }, { colspan: 1 }]}>
+        <div></div>
+        <SpaceBetween direction="vertical" size="l">
+
+        <Container textAlign='center'>
+          <Form
+            actions={
+              <SpaceBetween direction="horizontal" size="xs">
+                <Button variant='primary' onClick={() => {
+                  addEvent(newEvent); 
+                }}>Add Event</Button>
+              </SpaceBetween>
+            }
+          >
+              <SpaceBetween direction="vertical" size="l">
+                <FormField label="New Event">
+                  <Input value={newEvent} placeholder='Awesome Event' onChange={event => {
+                    setNewEvent(event.detail.value)
+                  }} />
+                </FormField>
+              </SpaceBetween>
+          </Form>
+          </Container>
+
+          <Table
+            columnDefinitions={[
+              {
+                id: "eventName",
+                header: "eventName",
+                cell: item => item.eventName || "-",
+                sortingField: "eventName"
+              },
+              {
+                id: "eventId",
+                header: "eventId",
+                cell: item => item.eventId || "-",
+                sortingField: "eventId"
+              },
+              {
+                id: "createdAt",
+                header: "createdAt",
+                cell: item => item.createdAt || "-",
+                sortingField: "createdAt"
+              }
+            ]}
+            items={events}
+            loadingText="Loading resources"
+            sortingDisabled
+            empty={
+              <Alert
+                visible={true}
+                dismissAriaLabel="Close alert"
+                header="No Events"
+              >
+                Please create your first event
+              </Alert>
+            }
+            header={
+              <Header>Events</Header>
+            }
+          />  
+        </SpaceBetween>
+        <div></div>
+      </Grid>
+    </>
+  )
+}
