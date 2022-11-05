eb340ac57b43b857487788730ce3844085115f44
Steven Askwith <askwith@amazon.com>
2022-11-05 10:01:47 +0000

    debug logging

--- backend/deepracer_event_manager_pipeline_stack.py
+++ backend/deepracer_event_manager_pipeline_stack.py
@@ -119,6 +119,7 @@ class CdkServerlessCharityPipelineStack(Stack):
                     "appsyncId=`cat appsyncId.txt` && aws appsync get-introspection-schema --api-id $appsyncId --format SDL ./website/src/graphql/schema.graphql",
                     "cd ./website/src/graphql",
 	                "npx amplify codegen",
+                    "ls -lah",
                     "cd ../..",
                     "docker run --rm -v $(pwd):/foo -w /foo public.ecr.aws/sam/build-nodejs16.x bash -c 'npm install --cache /tmp/empty-cache && npm run build'",
                     "aws s3 sync ./build/ s3://$sourceBucketName/ --delete",
