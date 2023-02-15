a6eb5633d510cbfcb9bd6809d44e9a22776ba498
Steven Askwith <askwith@amazon.com>
2023-02-15 12:00:55 +0000

    removed try catch to pass error to AppSync which will handle the lambda generated erros gracefully

--- lib/lambdas/users_function/index.py
+++ lib/lambdas/users_function/index.py
@@ -26,6 +26,12 @@ def json_serial(obj):
     raise TypeError("Type %s not serializable" % type(obj))
 
 
+def clean_json(obj):
+    temp = json.dumps(obj, default=json_serial)  # sort out datetime
+    temp2 = json.loads(temp)
+    return temp2
+
+
 @logger.inject_lambda_context(correlation_id_path=correlation_paths.APPSYNC_RESOLVER)
 @tracer.capture_lambda_handler
 def lambda_handler(event, context):
@@ -34,67 +40,51 @@ def lambda_handler(event, context):
 
 @app.resolver(type_name="Query", field_name="listUsers")
 def listUsers():
-    try:
-        # TODO: Probably need to change this to a paging request so the frontend
-        #       can send a request for the next page
+    # TODO: Probably need to change this to a paging request so the frontend
+    #       can send a request for the next page
 
-        paginator = client_cognito.get_paginator("list_users")
-        response_iterator = paginator.paginate(
-            UserPoolId=user_pool_id,
-            PaginationConfig={
-                "PageSize": 30,
-            },
-        )
+    paginator = client_cognito.get_paginator("list_users")
+    response_iterator = paginator.paginate(
+        UserPoolId=user_pool_id,
+        PaginationConfig={
+            "PageSize": 30,
+        },
+    )
 
-        users = []
-        for r in response_iterator:
-            users.append(r["Users"])
-            # send batch of results to appsync end point...
+    users = []
+    for r in response_iterator:
+        users.append(r["Users"])
+        # send batch of results to appsync end point...
 
-        # Squash the list of lists
-        # Won't need to do this once we are sending
-        # batches of results to appsync end point...
-        all_users = [item for sublist in users for item in sublist]
-        # logger.info(all_users)
+    # Squash the list of lists
+    # Won't need to do this once we are sending
+    # batches of results to appsync end point...
+    all_users = [item for sublist in users for item in sublist]
+    # logger.info(all_users)
 
-        # pull "sub" out to top level of user object
-        for user in all_users:
-            for attributes in user["Attributes"]:
-                if attributes["Name"] == "sub":
-                    # logger.info(attributes["Value"])
-                    user["sub"] = attributes["Value"]
+    # pull "sub" out to top level of user object
+    for user in all_users:
+        for attributes in user["Attributes"]:
+            if attributes["Name"] == "sub":
+                # logger.info(attributes["Value"])
+                user["sub"] = attributes["Value"]
 
-        temp = json.dumps(all_users, default=json_serial)  # sort out datetime
-        temp2 = json.loads(temp)
-        # logger.info(temp2)
-        return temp2
-        # return "submitted request"
-
-    except Exception as error:
-        logger.exception(error)
-        return error
+    return clean_json(all_users)
+    # return "submitted request"
 
 
 @app.resolver(type_name="Mutation", field_name="createUser")
 def create_user(username: str, email: str):
-    try:
-        user = client_cognito.admin_create_user(
-            UserPoolId=user_pool_id,
-            Username=username,
-            UserAttributes=[
-                {"Name": "email", "Value": email},
-            ],
-            DesiredDeliveryMediums=[
-                "EMAIL",
-            ],
-        )
+    user = client_cognito.admin_create_user(
+        UserPoolId=user_pool_id,
+        Username=username,
+        UserAttributes=[
+            {"Name": "email", "Value": email},
+        ],
+        DesiredDeliveryMediums=[
+            "EMAIL",
+        ],
+    )
 
-        temp = json.dumps(user, default=json_serial)  # sort out datetime
-        temp2 = json.loads(temp)
-        logger.info(temp2)
-        return temp2
-        # return "submitted request"
-
-    except Exception as error:
-        logger.exception(error)
-        return error
+    logger.info(user["User"])
+    return clean_json(user["User"])
