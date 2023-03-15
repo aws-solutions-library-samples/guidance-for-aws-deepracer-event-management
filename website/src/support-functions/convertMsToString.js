da723e313ccee91d84d119fd6180473a2a385e63
Marco Buss <marbuss@amazon.de>
2023-03-15 16:48:25 +0000

    Lap stats from actual racer working

--- website/src/support-functions/convertMsToString.js
+++
@@ -1,8 +0,0 @@
-export function convertMsToString(timeInMS) {
-  const millisecondsAsString = String(Math.floor(timeInMS % 1000)).padStart(3, '0');
-  const secondsAsString = String(Math.floor(timeInMS / 1000)).padStart(2, '0');
-  const seconds = Math.floor(timeInMS / 1000);
-  const minutesAsString = String(Math.floor(seconds / 60)).padStart(2, '0');
-  const timeAsString = `${minutesAsString}:${secondsAsString}.${millisecondsAsString}`;
-  return timeAsString;
-}
