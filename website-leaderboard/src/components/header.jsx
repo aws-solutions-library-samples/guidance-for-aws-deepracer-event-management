37e6f03b9ac378788749124adb09d4690a19b07b
Daniel Lindner <danlndnr@amazon.de>
2023-03-15 12:06:42 +0000

    Resolve "Leaderboard QR code to itself"

--- website-leaderboard/src/components/header.jsx
+++ website-leaderboard/src/components/header.jsx
@@ -3,12 +3,24 @@ import React from 'react';
 // import { useTranslation } from 'react-i18next';
 import Logo from '../assets/logo.png';
 import styles from './header.module.css';
+import { QRCodeSVG } from 'qrcode.react';
 
 const Header = ({ headerText }) => {
     return (
         <div className={styles.headerRoot}>
             <img src={Logo} alt="DeepRacer Logo" className={styles.logo} />
             <div className={styles.headerText}>{headerText}</div>
+          <div>
+            <QRCodeSVG value={window.location.href}
+                       level='M'
+                       imageSettings={{
+                         src: Logo,
+                         excavate: false,
+                         height: 40,
+                         width: 40,
+                       }}
+            />
+          </div>
         </div>
     );
 };
