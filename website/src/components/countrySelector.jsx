628b90440644f8a9f2807c71876fee45f5a6bd9f
Steven Askwith <askwith@amazon.com>
2023-03-15 11:38:27 +0000

    Added country to manual sign up

--- website/src/components/countrySelector.jsx
+++ website/src/components/countrySelector.jsx
@@ -1,3 +1,4 @@
+import { SelectField } from '@aws-amplify/ui-react';
 import { FormField, Select } from '@cloudscape-design/components';
 import { getNames, registerLocale } from 'i18n-iso-countries';
 import React, { useEffect, useState } from 'react';
@@ -15,26 +16,50 @@ export function CountrySelector(props) {
         );
     }, []);
 
+    const [countryOptionsAmplify, setCountryOptionsAmplify] = useState();
+
+    // Populate country options for select dropdown
+    useEffect(() => {
+        registerLocale(require('i18n-iso-countries/langs/en.json'));
+        setCountryOptionsAmplify(
+            Object.entries(getNames('en', { select: 'official' })).map((_countryCode) => {
+                return <option key={_countryCode[0]} value={_countryCode[0]} label={_countryCode[1]}></option> ;
+            })
+        );
+    }, []);
+
     const GetCountryOptionFromId = (id) => {
         if (countryOptions) {
             return countryOptions.find((option) => option.value === id);
         }
     };
 
-    return (
-        <FormField
-            label={props.label}
-            description={props.description}
-        >
-            <Select
-                selectedOption={GetCountryOptionFromId(props.countryCode)}
-                onChange={({ detail }) => {
-                    props.setCountryCode(detail.selectedOption.value);
-                }}
-                options={countryOptions}
-                selectedAriaLabel="Selected"
-                filteringType="auto"
-            />
-        </FormField>
-    )
+    if(props.amplify === true){
+        return (
+            <SelectField 
+                name="custom:countryCode" 
+                placeholder="Select country..."
+            >
+                {countryOptionsAmplify}
+            </SelectField>
+        )
+    } else {
+        return (
+            <FormField
+                label={props.label}
+                description={props.description}
+            >
+                <Select
+                    selectedOption={GetCountryOptionFromId(props.countryCode)}
+                    onChange={({ detail }) => {
+                        props.setCountryCode(detail.selectedOption.value);
+                    }}
+                    options={countryOptions}
+                    selectedAriaLabel="Selected"
+                    filteringType="auto"
+                    placeholder="Country"
+                />
+            </FormField>
+        )
+    }
 }
