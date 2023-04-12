ea0a1508912f0306d089c77b652cd5ed46f12895
David Smith <dasmthc@amazon.co.uk>
2023-04-12 15:01:07 +0000

    Translation files

---
+++ docs/DOCS.md
@@ -0,0 +1,139 @@
+# DREM
+
+**Note:** This documentation assumes that DREM is already installed and running.
+
+## Definitions
+
+Event - A time period where a number of racers will run their models on a physical track in a competition
+
+Race - Single run by a racer based on the number of minuted as defined by the event
+
+Fleet - Collection of cars, managed (and used) by a single person / team
+
+## User levels
+
+Admin - Can do anything - With great power comes great responsibility
+
+Commentator -
+
+Operator -
+
+Registration - User who is able to register racers, used at larger events where people who haven't trained a model are more likely to be present and want to race using one of the event sample models
+
+Racer - Someone who registers an account or has an account created for them, can upload and remove their models to / from DREM
+
+## Welcome to DREM
+
+![Homepage](./images/drem-home-admin.png)
+DREM homepage when logged in as an admin user
+
+## General interface notes
+
+All tables can be filtered and support pagination where there are more entries available. Clicking on the settings cog next to the pagination information for the table will open a modal presenting options to personalise the page size. Filtering works across all visible columns for the table data.
+
+# Operator
+
+## Car management
+
+### Fleets
+
+-   Permissions required: Admin, Operator
+-   Navigation path: Operator -> Car management -> Fleets
+
+![Fleets admin](./images/drem-fleets-admin.png)
+
+#### Create a fleet
+
+1. To create a new fleet click "Create" at the top of the table on the right hand side side
+
+![Create fleet](./images/drem-fleets-create-button.png)
+
+2. This will start the create fleet process
+
+![Name fleet](./images/drem-fleets-create-1.png)
+
+3. Give your fleet a name, and if you already have cars activiated with DREM you can add them to the fleet (you can also add / remove to the fleet later if you wish)
+
+4. Press "Create fleet" to complete the process
+
+![Name fleet](./images/drem-fleets-create-2.png)
+
+5. Your newly created fleet will now be visible in the list of fleets
+
+![List of fleets](./images/drem-fleets-create-3.png)
+
+#### Edit a fleet
+
+1. Select the fleet you want to edit
+
+2. Once you have selected a fleet the "Edit" button is enabled
+
+![Edit fleet](./images/drem-fleets-edit-1.png)
+
+3. In the edit screen, you can change the name of the fleet and also add / remove cars from the fleet, once you've made changes click on "Save changes" to return back to the fleet list
+
+![Edit fleet](./images/drem-fleets-edit-2.png)
+
+#### Delete a fleet
+
+1. Select the fleet you want to delete
+
+2. Once you have selected a fleet the "Delete" button is enabled. Should you need to you can also select more than one fleet
+
+![Delete fleet](./images/drem-fleets-edit-1.png)
+
+3. You'll be promted to confirm that you want to delete the fleet, clicking "Delete" will remove the selected fleets from DREM and you'll be returned to the "Fleets admin" screen
+
+### Cars
+
+### Car activation
+
+## Event management
+
+### Events
+
+-   Permissions required: Admin, Operator
+-   Navigation path: Operator -> Event management -> Events
+
+![Events admin](./images/drem-events-admin.png)
+
+### Create an event
+
+1. To create a new event click "Create" at the top of the table on the right hand side
+
+![Create event](./images/drem-events-create.png)
+
+2. This will start the create event process and you'll be presented with the form below
+
+![Create event](./images/drem-events-create-1.png)
+
+3. Below is a completed example for the 2023 London Summit. **note:** Race customizations is closed by default
+
+![Create event](./images/drem-events-create-2.png)
+
+4. Once you have filled in all of the event information click "Click event" The event will be created and you will be return to the list of events with the newly created event visible in the list
+
+![Create event](./images/drem-events-create-3.png)
+
+### Manage race
+
+-   Permissions required: Admin, Operator
+-   Navigation path: Operator -> Event management -> Events
+
+![Events admin](./images/drem-race-admin.png)
+
+## FAQ
+
+## Documentation notes
+
+Screenshots were taken on a 16" MBP using [Moom](https://manytricks.com/moom/) to create a [Firefox](https://www.mozilla.org/) window size of 4x4 grid. Full length screenshots were created using the "[Screenshot](https://support.mozilla.org/en-US/kb/take-screenshots-firefox)" feature of Firefox. Screenshot resolution is 2390x2020 (for a standard length page)
+
+Page size was set to "10"
+
+Screen shots showing button locations are left full width, but cropped horizontally
+
+Highlight boxes / text and arrows are all in red (#FF0000) with a 4pt stroke, square corners, font is Amazon Ember, 64pt
+
+The default table visible columns are used unless showing a specfic value
+
+Screenshots are placed below the task, where used numbers to highlight areas on a screen shot are place on the top left corner
--- website/src/admin/events/trackConfigPanel.jsx
+++ website/src/admin/events/trackConfigPanel.jsx
@@ -1,216 +1,157 @@
 import {
-    Container,
-    ExpandableSection,
-    FormField,
-    Header,
-    Select,
-    SpaceBetween,
+  Container,
+  ExpandableSection,
+  FormField,
+  Header,
+  Select,
+  SpaceBetween,
 } from '@cloudscape-design/components';
 import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import {
-    GetRaceTimeOptionFromId,
-    GetRankingOptionFromId,
-    GetResetOptionFromId,
-    GetTrackOptionFromId,
-    RaceTimeConfig,
-    RaceTypeConfig,
-    ResetConfig,
-    TrackTypeConfig,
+  GetRaceTimeOptionFromId,
+  GetRankingOptionFromId,
+  GetResetOptionFromId,
+  GetTrackOptionFromId,
+  RaceTimeConfig,
+  RaceTypeConfig,
+  ResetConfig,
+  TrackTypeConfig,
 } from './raceConfig';
 
 const RaceCustomizationsFooter = (props) => {
-    const [custIsExpanded, setCustIsExpanded] = useState(false);
+  const { t } = useTranslation();
+  const [custIsExpanded, setCustIsExpanded] = useState(false);
 
-    // Keep race customizations open after user click on included items
-    const custIsExpandedHandler = () => {
-        setCustIsExpanded((prevState) => !prevState);
-    };
+  // Keep race customizations open after user click on included items
+  const custIsExpandedHandler = () => {
+    setCustIsExpanded((prevState) => !prevState);
+  };
 
-    return (
-        <ExpandableSection
-            headerText="Race customizations"
-            expanded={custIsExpanded}
-            variant="footer"
-            onChange={custIsExpandedHandler}
-        >
-            {props.children}
-        </ExpandableSection>
-    );
+  return (
+    <ExpandableSection
+      headerText={t('events.race.race-customisations')}
+      expanded={custIsExpanded}
+      variant="footer"
+      onChange={custIsExpandedHandler}
+    >
+      {props.children}
+    </ExpandableSection>
+  );
 };
 
 const DefaultRacingFooter = ({
-    numberOfResetsPerLap,
-    raceTimeInMin,
-    onChange,
-    raceTimeOptions,
-    resetOptions,
+  numberOfResetsPerLap,
+  raceTimeInMin,
+  onChange,
+  raceTimeOptions,
+  resetOptions,
 }) => {
-    const { t } = useTranslation();
-    return (
-        <SpaceBetween size="l">
-            <FormField
-                label={t('events.race.race-time')}
-                description={t('events.race.race-time-description')}
-            >
-                <Select
-                    selectedOption={GetRaceTimeOptionFromId(raceTimeInMin)}
-                    onChange={({ detail }) =>
-                        onChange({ raceTimeInMin: detail.selectedOption.value })
-                    }
-                    options={raceTimeOptions}
-                    selectedAriaLabel="Selected"
-                    filteringType="auto"
-                />
-            </FormField>
-            <FormField
-                label={t('events.race.resets-per-lap')}
-                description={t('events.race.resets-per-lap-description')}
-            >
-                <Select
-                    selectedOption={GetResetOptionFromId(numberOfResetsPerLap)}
-                    onChange={({ detail }) =>
-                        onChange({ numberOfResetsPerLap: detail.selectedOption.value })
-                    }
-                    options={resetOptions}
-                    selectedAriaLabel="Selected"
-                    filteringType="auto"
-                />
-            </FormField>
-        </SpaceBetween>
-    );
+  const { t } = useTranslation();
+  return (
+    <SpaceBetween size="l">
+      <FormField
+        label={t('events.race.race-time')}
+        description={t('events.race.race-time-description')}
+      >
+        <Select
+          selectedOption={GetRaceTimeOptionFromId(raceTimeInMin)}
+          onChange={({ detail }) => onChange({ raceTimeInMin: detail.selectedOption.value })}
+          options={raceTimeOptions}
+          selectedAriaLabel="Selected"
+          filteringType="auto"
+        />
+      </FormField>
+      <FormField
+        label={t('events.race.resets-per-lap')}
+        description={t('events.race.resets-per-lap-description')}
+      >
+        <Select
+          selectedOption={GetResetOptionFromId(numberOfResetsPerLap)}
+          onChange={({ detail }) => onChange({ numberOfResetsPerLap: detail.selectedOption.value })}
+          options={resetOptions}
+          selectedAriaLabel="Selected"
+          filteringType="auto"
+        />
+      </FormField>
+    </SpaceBetween>
+  );
 };
 
-// const FinishXLapsFooter = ({ lapsToFinish, onChange, lapsToFinishOptions }) => {
-//     const { t } = useTranslation();
-//     return (
-//         <SpaceBetween size="l">
-//             <FormField
-//                 label={t('events.race.laps-to-finish')}
-//                 description={t('events.race.laps-to-finish-description')}
-//             >
-//                 <Select
-//                     selectedOption={GetLapsOptionFromId(lapsToFinish)}
-//                     onChange={({ detail }) =>
-//                         onChange({ lapsToFinish: detail.selectedOption.value })
-//                     }
-//                     options={lapsToFinishOptions}
-//                     selectedAriaLabel="Selected"
-//                     filteringType="auto"
-//                 />
-//             </FormField>
-//         </SpaceBetween>
-//     );
-// };
-
 export const TrackConfigPanel = ({
-    trackId,
-    numberOfResetsPerLap,
-    raceTimeInMin,
-    trackType,
-    rankingMethod,
-    onChange,
+  trackId,
+  numberOfResetsPerLap,
+  raceTimeInMin,
+  trackType,
+  rankingMethod,
+  onChange,
 }) => {
-    const raceTimeOptions = RaceTimeConfig();
-    const raceRankingOptions = RaceTypeConfig();
-    const resetOptions = ResetConfig();
-    const trackOptions = TrackTypeConfig();
-    const { t } = useTranslation();
+  const raceTimeOptions = RaceTimeConfig();
+  const raceRankingOptions = RaceTypeConfig();
+  const resetOptions = ResetConfig();
+  const trackOptions = TrackTypeConfig();
+  const { t } = useTranslation();
 
-    const UpdateConfig = (attr) => {
-        onChange({ raceConfig: { ...attr } });
-    };
+  const UpdateConfig = (attr) => {
+    onChange({ raceConfig: { ...attr } });
+  };
 
-    const [raceCustomizationsFooter, setRaceCustomizationsFooter] = useState(
-        <DefaultRacingFooter
-            numberOfResetsPerLap={numberOfResetsPerLap}
-            raceTimeInMin={raceTimeInMin}
-            onChange={UpdateConfig}
-            resetOptions={resetOptions}
-            raceTimeOptions={raceTimeOptions}
-        />
+  const [raceCustomizationsFooter, setRaceCustomizationsFooter] = useState(
+    <DefaultRacingFooter
+      numberOfResetsPerLap={numberOfResetsPerLap}
+      raceTimeInMin={raceTimeInMin}
+      onChange={UpdateConfig}
+      resetOptions={resetOptions}
+      raceTimeOptions={raceTimeOptions}
+    />
+  );
+
+  // Select race customizations footer
+  useEffect(() => {
+    setRaceCustomizationsFooter(
+      <DefaultRacingFooter
+        numberOfResetsPerLap={numberOfResetsPerLap}
+        raceTimeInMin={raceTimeInMin}
+        onChange={UpdateConfig}
+        resetOptions={resetOptions}
+        raceTimeOptions={raceTimeOptions}
+      />
     );
+    // }
+  }, [numberOfResetsPerLap, raceTimeInMin]);
 
-    // // Set default value on load if fields are empty
-    // useEffect(() => {
-    //     const raceConfig = {};
-
-    //     if (!numberOfResetsPerLap) {
-    //         raceConfig['numberOfResetsPerLap'] = resetOptions[0].value;
-    //     }
-    //     // if (!lapsToFinish) {
-    //     //     raceConfig['lapsToFinish'] = lapOptions[4].value;
-    //     // }
-    //     if (!raceTimeInMin) {
-    //         raceConfig['raceTimeInMin'] = raceTimeOptions[2].value;
-    //     }
-    //     if (!rankingMethod) {
-    //         raceConfig['rankingMethod'] = raceRankingOptions[0].value;
-    //     }
-    //     // if (raceConfig) {
-    //     //     UpdateConfig(raceConfig);
-    //     // }
-    // }, [numberOfResetsPerLap, rankingMethod, raceTimeInMin]);
-
-    // Select race customizations footer
-    useEffect(() => {
-        // if (rankingMethod && rankingMethod === 'FINISH_X_LAPS') {
-        //     setRaceCustomizationsFooter(
-        //         <FinishXLapsFooter
-        //             lapsToFinish={lapsToFinish}
-        //             onChange={UpdateConfig}
-        //             lapsToFinishOptions={lapOptions}
-        //         />
-        //     );
-        // } else {
-        setRaceCustomizationsFooter(
-            <DefaultRacingFooter
-                numberOfResetsPerLap={numberOfResetsPerLap}
-                raceTimeInMin={raceTimeInMin}
-                onChange={UpdateConfig}
-                resetOptions={resetOptions}
-                raceTimeOptions={raceTimeOptions}
-            />
-        );
-        // }
-    }, [numberOfResetsPerLap, raceTimeInMin]);
-
-    // JSX
-    return (
-        <Container
-            header={<Header variant="h2">Race settings</Header>}
-            footer={<RaceCustomizationsFooter>{raceCustomizationsFooter}</RaceCustomizationsFooter>}
+  // JSX
+  return (
+    <Container
+      header={<Header variant="h2">Race settings</Header>}
+      footer={<RaceCustomizationsFooter>{raceCustomizationsFooter}</RaceCustomizationsFooter>}
+    >
+      <SpaceBetween size="xl">
+        <FormField
+          label={t('events.tracks.choose')}
+          description={t('events.tracks.choose-description')}
         >
-            <SpaceBetween size="xl">
-                <FormField
-                    label={t('events.tracks.choose')}
-                    description={t('events.tracks.choose-description')}
-                >
-                    <Select
-                        selectedOption={GetTrackOptionFromId(trackType)}
-                        onChange={({ detail }) =>
-                            UpdateConfig({ trackType: detail.selectedOption.value })
-                        }
-                        options={trackOptions}
-                        selectedAriaLabel="Selected"
-                        filteringType="auto"
-                    />
-                </FormField>
-                <FormField
-                    label={t('events.race.ranking-method')}
-                    description={t('events.race.ranking-method-description')}
-                >
-                    <Select
-                        selectedOption={GetRankingOptionFromId(rankingMethod)}
-                        onChange={({ detail }) =>
-                            UpdateConfig({ rankingMethod: detail.selectedOption.value })
-                        }
-                        options={raceRankingOptions}
-                        selectedAriaLabel="Selected"
-                        filteringType="auto"
-                    />
-                </FormField>
-            </SpaceBetween>
-        </Container>
-    );
+          <Select
+            selectedOption={GetTrackOptionFromId(trackType)}
+            onChange={({ detail }) => UpdateConfig({ trackType: detail.selectedOption.value })}
+            options={trackOptions}
+            selectedAriaLabel="Selected"
+            filteringType="auto"
+          />
+        </FormField>
+        <FormField
+          label={t('events.race.ranking-method')}
+          description={t('events.race.ranking-method-description')}
+        >
+          <Select
+            selectedOption={GetRankingOptionFromId(rankingMethod)}
+            onChange={({ detail }) => UpdateConfig({ rankingMethod: detail.selectedOption.value })}
+            options={raceRankingOptions}
+            selectedAriaLabel="Selected"
+            filteringType="auto"
+          />
+        </FormField>
+      </SpaceBetween>
+    </Container>
+  );
 };
