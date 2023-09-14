8d5d703576c34c95577f33e3220cf4ac6c805d8d
Johan Esbjorner <esbjj@amazon.com>
2023-09-14 18:23:57 +0000

    Main - Resolve "model upload sporadically not working"

--- lib/lambdas/models_md5/index.py
+++ lib/lambdas/models_md5/index.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from tempfile import TemporaryDirectory
 
 import boto3
+import dynamo_helpers
 import http_response
 import simplejson as json
 from aws_lambda_powertools import Logger
@@ -46,60 +47,95 @@ def lambda_handler(event: dict, context: LambdaContext) -> str:
     model_filename_parts = model_filename.split(".")
     model_name = model_filename_parts[0]
 
-    with TemporaryDirectory() as tmpdir:
-        model_filename_full_path = os.path.join(tmpdir, model_filename)
-        model_name_full_path = os.path.join(tmpdir, model_name)
-        modelpb_path_name = os.path.join(model_name_full_path, "agent/model.pb")
-        modelmeta_path_name = os.path.join(model_name_full_path, "model_metadata.json")
+    item = {
+        "racerName": racer_name,
+        "racerIdentityId": racer_identity_id,
+        "md5Datetime": datetime.utcnow().isoformat() + "Z",
+        "modelMD5": None,
+        "modelMetadataMD5": None,
+        "sensor": None,
+        "trainingAlgorithm": None,
+        "actionSpaceType": None,
+    }
 
-        # Get the MD5 of model elements and update the DB
-        try:
+    try:
+        with TemporaryDirectory() as tmpdir:
+            model_filename_full_path = os.path.join(tmpdir, model_filename)
+            model_name_full_path = os.path.join(tmpdir, model_name)
+            modelpb_path_name = os.path.join(model_name_full_path, "agent/model.pb")
+            modelmeta_path_name = os.path.join(
+                model_name_full_path, "model_metadata.json"
+            )
+
+            # Get the MD5 of model elements and update the DB
             s3.download_file(bucket, model_key, model_filename_full_path)
             tar = tarfile.open(model_filename_full_path)
             tar.extractall(model_name_full_path)
             tar.close()
 
+            logger.info(os.listdir(model_name_full_path))
             # Get the MD5
-            model_md5 = md5_file(modelpb_path_name)
-            logger.debug(f"{modelpb_path_name} MD5 => {model_md5}")
+            try:
+                model_md5 = md5_file(modelpb_path_name)
+                logger.debug(f"{modelpb_path_name} MD5 => {model_md5}")
+                item["modelMD5"] = model_md5
+            except Exception as error:
+                logger.warn(error)
 
-            model_metadata_md5 = md5_file(modelmeta_path_name)
-            logger.debug(f"{modelmeta_path_name} MD5 => {model_metadata_md5}")
+            try:
+                model_metadata_md5 = md5_file(modelmeta_path_name)
+                logger.debug(f"{modelmeta_path_name} MD5 => {model_metadata_md5}")
+                item["modelMetadataMD5"] = model_metadata_md5
+            except Exception as error:
+                logger.warn(error)
 
-            # Get sensor, training algorithm and action space from model_metadata.jsom
-            with open(modelmeta_path_name) as json_file:
-                model_metadata_contents = json_file.read()
+            # Get sensor, training algorithm and action space from model_metadata.json
+            try:
+                with open(modelmeta_path_name) as json_file:
+                    model_metadata_contents = json_file.read()
 
-            logger.debug(f"model_metadata_content => {model_metadata_contents}")
+                logger.debug(f"model_metadata_content => {model_metadata_contents}")
 
-            model_metadata_json = json.loads(model_metadata_contents)
+                model_metadata_json = json.loads(model_metadata_contents)
+                try:
+                    item["sensor"] = model_metadata_json["sensor"]
+                except Exception as error:
+                    logger.warn(error)
+
+                try:
+                    item["trainingAlgorithm"] = model_metadata_json[
+                        "training_algorithm"
+                    ]
+                except Exception as error:
+                    logger.warn(error)
+
+                try:
+                    item["actionSpaceType"] = model_metadata_json["action_space_type"]
+                except Exception as error:
+                    logger.warn(error)
+            except Exception as error:
+                logger.warn(error)
+
+            ddb_update_expressions = dynamo_helpers.generate_update_query(item)
+
             response = table.update_item(
                 Key={
                     "modelId": model_id,
                 },
-                UpdateExpression=(
-                    "SET racerName = :racerName, racerIdentityId = :racerIdentityId,"
-                    " md5Datetime = :md5Datetime, modelMD5 = :modelMD5,"
-                    " modelMetadataMD5 = :modelMetadataMD5, sensor = :sensor,"
-                    " trainingAlgorithm = :trainingAlgorithm, actionSpaceType ="
-                    " :actionSpaceType"
-                ),
-                ExpressionAttributeValues={
-                    ":racerName": racer_name,
-                    ":racerIdentityId": racer_identity_id,
-                    ":md5Datetime": datetime.utcnow().isoformat() + "Z",
-                    ":modelMD5": model_md5,
-                    ":modelMetadataMD5": model_metadata_md5,
-                    ":sensor": model_metadata_json["sensor"],
-                    ":trainingAlgorithm": model_metadata_json["training_algorithm"],
-                    ":actionSpaceType": model_metadata_json["action_space_type"],
-                },
+                UpdateExpression=ddb_update_expressions["UpdateExpression"],
+                ExpressionAttributeNames=ddb_update_expressions[
+                    "ExpressionAttributeNames"
+                ],
+                ExpressionAttributeValues=ddb_update_expressions[
+                    "ExpressionAttributeValues"
+                ],
+                ReturnValues="ALL_NEW",
             )
-            logger.debug(response)
+            logger.debug(response["Attributes"])
 
             # Return the MD5 for now
             return http_response.response(200)
 
-        except Exception as error:
-            logger.exception(error)
-            return http_response.response(500, error)
+    except Exception as error:
+        logger.exception(error)
+        return http_response.response(500, error)
