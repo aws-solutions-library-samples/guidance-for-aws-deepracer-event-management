2496dcd6edf30b14beada1355dc608670d658fdf
Lars Ludvigsen <larsll@outlook.com>
2025-06-01 19:52:46 +0200

    Fix #136

--- website/public/car_activation.sh
+++ website/public/car_activation.sh
@@ -144,12 +144,22 @@ DR_CAR_UPDATE()
 
     echo -e -n "\n- Updating DeepRacer car software...\n"
 
-    # Update ROS cert
-    curl https://repo.ros2.org/repos.key | apt-key add -
+    # Remove old ROS keys and config
+    echo -e -n "\n- Remove old ROS keys and config"
+    apt-key del "F42E D6FB AB17 C654"
+    rm -f /usr/share/keyrings/ros-archive-keyring.gpg
+    rm -f /etc/apt/sources.list.d/ros2-latest.list
+    
+    # Add new ROS repository package
+    echo -e -n "\n- Add new ROS repository package"
+    export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F\" '{print $4}')
+    curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo $UBUNTU_CODENAME)_all.deb"
+    apt install /tmp/ros2-apt-source.deb
 
     # Get latest key from OpenVINO
+    echo -e -n "\n- Get latest OpenVINO GPG key"
     curl -o GPG-PUB-KEY-INTEL-SW-PRODUCTS https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
-    sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS
+    apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS
 
     # Update Ubuntu - removed for now as it takes so long from the standard 20.04 build
     # echo -e -n "\n- Updating Ubuntu packages"
